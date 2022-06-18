98810630e6f4c495357856976de96d81b13d574d
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2022-06-18 10:10:30 -0300

    ...

--- src/domain/services/auth/authApi.ts
+++ src/domain/services/auth/authApi.ts
@@ -7,7 +7,7 @@ export class AuthApi {
     async validaToken(token: string): Promise<string> {
         return await axios.get('http://localhost:3002/auth/', {}, {
             headers: {
-                "x-access-token": token
+                "x-access-token": token ? token : ""
             }
         }).catch(() => {
             console.log("Serviço indisponível: AuthApi");
